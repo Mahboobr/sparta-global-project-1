b6732962a0303e3514fa0ddd1140fdd726f26efd
Tech-A28 <tech-a28@Tech-A28s-MacBook-Air.local>
2018-02-07 11:27:39 +0000

    Ball moves much better

--- app.js
+++ app.js
@@ -1,183 +1,114 @@
+document.addEventListener('DOMContentLoaded', function() {
 
-$(document).ready(function(){
+
 
   var bricks = document.getElementById('bricks');
-  var bricksList = document.getElementsByClassName('cols');
   var rows = [];
   var cols = [];
   var ball = document.getElementById('ball');
   var paddle = document.getElementById('paddle');
-  var Score = document.getElementById('Score');
-  var Lives = document.getElementById('Lives');
-  var pos = 0;
-  var posX = 0;
-  var posY = 30;
+  var pos = 0;  //paddle pos
+  // ball.style.left = $("#paddle").outerWidth() / 2; // these two set where the ball starts
+  // ball.style.bottom = $("#paddle").outerHeight() + 10;
+  var posX = 10; // these two set where the ball starts
+  var posY = 40;
   var lives = 3;
-  var score = 0;
 
   BrickLayout();
 
+  //BallControl();
   document.addEventListener("keydown", function(event) {
-
-    var col = document.getElementsByClassName('col');
-    if (event.which == 37) {    // arrow keys presses
+    if (event.which == 37) {
       var id = setInterval(MoveLeft, 1);
+      //BallControl();
+      // console.log(pos);
     }else if (event.which == 39) {
       var id = setInterval(MoveRight, 1);
+      //BallControl();
+      // console.log(pos);
     }
     document.addEventListener("keyup", function(event) {
       clearInterval(id);
+      // console.log(pos);
     });
   });
+  console.log($("#gameScreen").innerWidth());
+  console.log($("#gameScreen").innerHeight());
+  console.log($("#id84").innerWidth());
+  console.log($("#id84").innerHeight());
+  console.log($("#id84").outerWidth());
+  console.log($("#id84").outerHeight());
+  console.log($("#paddle").outerWidth());
+  console.log($("#ball").outerWidth());
 
-//fully working code
-$("#ball").on("click", function() {
-  var posX = 10;
-  var posY = 40;
-  var dir = 1;
-  var interval = setInterval(frame, 10);
-  var Br84on = 1;  // brick 84 flag
-  var Xarr = [1, 3]; // array for x pos's
-  var Yarr = [1, 3];
-  var LRFLag = 1;
+  // var posX = 10;
+  // var posY = 40;
+  var xSpeed = 2;
+  var ySpeed = 1;
+  $("#ball").on("click", function() {
+    var interval = setInterval(frame, 10);
 
-  function frame() {
-
-    Xarr.push(posX);
-    var Xans = Xarr[1] - Xarr[0];
-    console.log("X " + Xans);
-    Xarr.shift();
-
-    console.log(Xarr);
-    console.log(Yarr);
-
-    Yarr.push(posY);
-    var Yans = Yarr[1] - Yarr[0];
-    console.log("Y " + Yans);
-    Yarr.shift();
-
-    console.log(posX);
-
-    if ( posY < 0 ) {
-      clearInterval(interval);
-
-      lives--;
-      console.log(lives);
-      $("#Lives").html("Lives = " + lives);
-
-    }else if (posX < 940 && posY < 700) {
-      if (Xans > 0 && Yans > 0 && LRFLag == 1 ) { // goes toward right wall
-        RightAndUp();
-        console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+    function frame() {
+      if ( posY < 0 ) {
+        clearInterval(interval);
       }
-      if (Xans <= 0 && Yans > 0 ) {   //hits right wall
-        LeftAndUp();
-        console.log("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
-      }
-      if (Xans <= 0 && Yans <= 0  ) {   // hits top wall
-        LeftAndDown();
-        console.log("ccccccccccccccccccccccccccccccccccccccccc");
-      }
-      if (Xans >= 0 && Yans <= 0 ) {   // hits left wall
-        RightAndDown();
-        console.log("dddddddddddddddddddddddddddddddddddddddddddd");
-      }
-      if (posX > 939) {
-        console.log("RIGHTRIGHTRIGHTRIGHTRIGHTRIGHTRIGHTRIGHTRIGHT");
-        LeftAndUp();
-        LRFLag = 0;
-      }
-      if (posY > 698) {
-        console.log("LeftLeftLeftLeftLeftLeftLeftLeftLeftLeftLeft");
-        RightAndDown();
-      }
-      if (posX == 0) {
-        console.log("DownDownDownDownDownDownDownDownDownDownDown");
-        RightAndDown();
-        LRFLag = 1;
-      }
-      if (posX > pos && posX < (pos + 96) && posY < 30 ) {    // hits paddle
-        LeftAndUp();
-      }
-      if (Br84on == 1) {
-        if (posX > 840 && posX < (840 + 96) && posY > 470 && posY > 480 ) {    // hits brick 84
-          var Brick84 = document.getElementById('id84');
-          console.log(Brick84);
-          Brick84.classList.remove('col');
-          Brick84.classList.add('hidden-col');
-          LeftAndDown();
-          score++;
-          LRFLag == 0;
-          Br84on = 0; // trying to disable this area sp ball can pass through
-        }
-      }
-      $("#Score").html("Score = " + score);
-    }//end of that else if
-  }
-
-  function RightAndUp() {
-    posX+=2;
-    posY++;
-    $("#ball").css({
-      "left": posX + "px",
-      "bottom": posY + "px"
-    });
-  }
-  function LeftAndUp() {
-    posX-=2;
-    posY++;
-    $("#ball").css({
-      "left": posX + "px",
-      "bottom": posY + "px"
-    });
-  }
-  function LeftAndDown() {
-    posX-=2;
-    posY-=2;
-    $("#ball").css({
-      "left": posX + "px",
-      "bottom": posY + "px"
-    });
-  }
-  function RightAndDown() {
-    posX+=2;
-    posY-=2;
-    $("#ball").css({
-      "left": posX + "px",
-      "bottom": posY + "px"
-    });
-  }
-})
-
-function BrickLayout() {
-  for (var i=0; i <5; i++) {
-    rows[i] = document.createElement('div');
-    rows[i].setAttribute('class','row');
-    bricks.appendChild(rows[i]);
-
-    for (var j=0; j<9; j++) {
-      cols[j] = document.createElement('div');
-      cols[j].setAttribute('class','col');
-      cols[j].setAttribute('id','id' + j + i);
-      cols[j].addEventListener('click',function(){
-        this.classList.remove('col');
-        this.classList.add('hidden-col');
+      posX+= xSpeed;
+      posY+= ySpeed;
+      $("#ball").css({
+        "left": posX + "px",
+        "bottom": posY + "px"
       });
-      rows[i].appendChild(cols[j]);
+      // if the position of ball is greater than the width of the playing area go left
+      if (posX > ($("#gameScreen").innerWidth() - $("#ball").outerWidth())) {  // width of ball is 20px set in css
+        xSpeed = -2;
+      }
+      // if the position of ball is greater than the height of the playing area go down
+      if (posY > ($("#gameScreen").innerHeight() - $("#ball").outerWidth())) {  // width of ball is 20px set in css
+        ySpeed = -1;
+      }// if the position of ball is less than the width of the playing area go right
+      if (posX <= 0) {
+        xSpeed = 2;
+      }// if the position of ball is the same as the paddle go up
+      if (posX > pos && posX < (pos + $("#paddle").outerWidth()) && posY < $("#paddle").outerHeight() + 10) { //+10 for paddle bottom margin
+        ySpeed = 1;
+      }
+
+    };
+
+  });
+
+  function BrickLayout() {
+    for (var i=0; i <5; i++) {
+      rows[i] = document.createElement('div');
+      rows[i].setAttribute('class','row');
+      bricks.appendChild(rows[i]);
+
+      for (var j=0; j<9; j++) {
+        cols[j] = document.createElement('div');
+        cols[j].setAttribute('class','col');
+        cols[j].setAttribute('id','id' + j + i);
+        cols[j].addEventListener('click',function(){
+          this.classList.remove('col');
+          this.classList.add('hidden-col');
+        });
+        rows[i].appendChild(cols[j]);
+      }
     }
   }
-}
-// controlling paddle
-function MoveLeft() {
-  if (pos > 0) {
-    pos--;
-    paddle.style.left = pos + 'px';
+
+  function MoveLeft() {
+    if (pos > 0) {
+      pos--;
+      paddle.style.left = pos + 'px';
+    }
   }
-}
-function MoveRight() {
-  if (pos < (960-96)) {
-    pos++;
-    paddle.style.left = pos + 'px';
+
+
+  function MoveRight() {
+    if (pos < (960-96)) {
+      pos++;
+      paddle.style.left = pos + 'px';
+    }
   }
-}
+
 });
