31c405429fafefca2542a5707f80a4a5d092469e
Tech-A28 <tech-a28@Tech-A28s-MacBook-Air.local>
2018-02-07 15:12:07 +0000

    Break More Blocks

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var ball = document.getElementById('ball');
   var paddle = document.getElementById('paddle');
   var pos = 0;  //paddle pos
-  // ball.style.left = $("#paddle").outerWidth() / 2; // these two set where the ball starts
-  // ball.style.bottom = $("#paddle").outerHeight() + 10;
+
   var posX = 10; // these two set where the ball starts
   var posY = 40;
   var lives = 3;
@@ -41,15 +40,19 @@ document.addEventListener('DOMContentLoaded', function() {
   console.log($("#paddle").outerWidth());
   console.log($("#ball").outerWidth());
 
-  // var posX = 10;
-  // var posY = 40;
-  var xSpeed = 2;
-  var ySpeed = 1;
+
+
   $("#ball").on("click", function() {
     var interval = setInterval(frame, 10);
+    var xSpeed = 2;
+    var ySpeed = 2;
+    var posX = 10;
+    var posY = 40;
 
     function frame() {
-      if ( posY < 0 ) {
+      if ( posY < -1 ) {
+        // ySpeed = 1;
+        console.log(posY);
         clearInterval(interval);
       }
       posX+= xSpeed;
@@ -73,9 +76,82 @@ document.addEventListener('DOMContentLoaded', function() {
         ySpeed = 1;
       }
 
-    };
+      console.log(($("#gameScreen").innerHeight() - $("#bricks").outerHeight()));
+      // Setting the area of where the ball collides with brick 84
+      if ( (posY >= ($("#gameScreen").innerHeight() - $("#bricks").outerHeight()) - ($("#ball").outerHeight()/2))
+      && (posY <= ($("#gameScreen").innerHeight() + $("#id84").outerHeight() - $("#bricks").outerHeight()) + ($("#ball").outerHeight()/2))
+      && (posX <= $("#gameScreen").innerWidth() - 4)
+      && (posX >= $("#gameScreen").innerWidth() - $("#id84").outerWidth() - 4)
+      && $("#id84").hasClass( "col" )
+    ){    //if conditions for brick 84 ^^
+        var Brick84 = document.getElementById('id84');
+        Brick84.classList.remove('col');
+        Brick84.classList.add('hidden-col');
+        console.log($("#id84"));
+        console.log(posX);
+        if (ySpeed == 1) {
+          ySpeed = -2;
+        }else {
+          ySpeed = 2;
+        };
+      }; // end of if for 84
 
-  });
+      if ( (posY >= ($("#gameScreen").innerHeight() - $("#bricks").outerHeight()) - ($("#ball").outerHeight()/2))
+      && (posY <= ($("#gameScreen").innerHeight() + $("#id74").outerHeight() - $("#bricks").outerHeight()) + ($("#ball").outerHeight()/2))
+      && (posX <= $("#gameScreen").innerWidth() - 4*2 - $("#id74").outerWidth())
+      && (posX >= $("#gameScreen").innerWidth() - $("#id74").outerWidth()*2 - 4*2)
+      && $("#id74").hasClass( "col" )
+    ){    //if conditions for brick 84 ^^
+        var Brick74 = document.getElementById('id74');
+        Brick74.classList.remove('col');
+        Brick74.classList.add('hidden-col');
+        console.log($("#id84"));
+        console.log(posX);
+        if (ySpeed == 1) {
+          ySpeed = -2;
+        }else {
+          ySpeed = 2;
+        };
+      }; // end of if for 84
+
+      if ( (posY >= ($("#gameScreen").innerHeight() - $("#bricks").outerHeight()) - ($("#ball").outerHeight()/2))
+      && (posY <= ($("#gameScreen").innerHeight() + $("#id64").outerHeight() - $("#bricks").outerHeight()) + ($("#ball").outerHeight()/2))
+      && (posX <= $("#gameScreen").innerWidth() - 4*3 - $("#id64").outerWidth())
+      && (posX >= $("#gameScreen").innerWidth() - $("#id64").outerWidth()*3 - 4*3)
+      && $("#id64").hasClass( "col" )
+    ){    //if conditions for brick 84 ^^
+        var Brick64 = document.getElementById('id64');
+        Brick64.classList.remove('col');
+        Brick64.classList.add('hidden-col');
+        console.log($("#id64"));
+        console.log(posX);
+        if (ySpeed == 1) {
+          ySpeed = -2;
+        }else {
+          ySpeed = 2;
+        };
+      }; // end of if for 84
+
+      if ( (posY >= ($("#gameScreen").innerHeight() + $("#id83").outerHeight() +10 - $("#bricks").outerHeight()) - ($("#ball").outerHeight()/2))
+      && (posY <= ($("#gameScreen").innerHeight() + $("#id83").outerHeight()*2 +10 - $("#bricks").outerHeight()) + ($("#ball").outerHeight()/2))
+      && (posX <= $("#gameScreen").innerWidth() - 4)
+      && (posX >= $("#gameScreen").innerWidth() - $("#id83").outerWidth() - 4)
+      && $("#id83").hasClass( "col" )
+    ){    //if conditions for brick 84 ^^
+        var Brick74 = document.getElementById('id83');
+        Brick74.classList.remove('col');
+        Brick74.classList.add('hidden-col');
+        console.log($("#id84"));
+        console.log(posX);
+        if (ySpeed == 1) {
+          ySpeed = -2;
+        }else {
+          ySpeed = 2;
+        };
+      }; // end of if for 84
+
+    }; // end of frame function
+  }); // end of click function
 
   function BrickLayout() {
     for (var i=0; i <5; i++) {
